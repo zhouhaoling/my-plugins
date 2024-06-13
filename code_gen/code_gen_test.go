602adcb435f7a747c9a0e913000fe811b854951f
zhou <zhl19174683866@163.com>
2024-06-13 17:30:59 +0800

    测试文件

--- code_gen/code_gen_test.go
+++ code_gen/code_gen_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestGenStruct(t *testing.T) {
 	//此处传入表名以及结构体名
-	GenStruct("ms_project", "Project")
+	//GenStruct("ms_project", "Project")
+	GenProtoMessage("ms_project", "ProjectMessage")
 	//可以在命令行中看到生成的结构体
 }
